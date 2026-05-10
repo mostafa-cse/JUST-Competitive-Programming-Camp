01ede1f4b70fdd75bec84d89cb1990e7ba94fdc8
Mostafa Kamal <100847323+mostafa-cse@users.noreply.github.com>
2026-05-10 02:21:45 +0000

    Add Problem C solution

---
+++ D - Vector-Sort.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    for (int &i : a) {
+        cin >> i;
+    }
+
+    sort(a.begin(), a.end());
+    // sort(begin(a), end(a)); // both are same
+
+    for (int &i : a) {
+        cout << i << " ";
+    }
+    return 0;
+}
